0bc5f6855dbc9477f348f8055adcc989c15d0a76
Son Nguyen <sonseo15072006@gmail.com>
2025-12-03 14:33:01 +0700

    chore: add favicon handling

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,22 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
+import footerLogo from "./components/assets/footer-logo.png";
 import "./index.css";
 
 const basename = import.meta.env.BASE_URL.replace(/\/+$/, "") || undefined;
+
+const faviconLink = document.querySelector<HTMLLinkElement>("#app-favicon") ??
+  document.querySelector<HTMLLinkElement>("link[rel~='icon']") ??
+  document.createElement("link");
+
+if (!faviconLink.parentNode) {
+  faviconLink.rel = "icon";
+  document.head.appendChild(faviconLink);
+}
+
+faviconLink.type = "image/png";
+faviconLink.href = footerLogo;
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter basename={basename}>
